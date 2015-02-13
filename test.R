ffcfcc68f4b476c5ef475b174544fbdc261a2653
Jonathan Bramble <jbramble82@gmail.com>
2015-02-13 14:01:17 +0000

    Updated plots for reports

--- test.R
+++ test.R
@@ -6,21 +6,26 @@ S <- Scatterer()  # create a scatterer object
 
 np <- Layer()     # and a layer representing the np
 lipid <- Layer()  # and other layers
+water <- Layer()
 na(S) <- 1.33     # set the ambient index
-d(np) <- 40       # and the size of the np in nm
-d(lipid) <- 45    # and the size of the other layers
-m(lipid) <- 1.45+0i   #fixed value
+d(np) <- 40       # and the size (radius) of the np in nm
+
+d(water) <- 41
+m(water) <- 1.33+0i
+
+d(lipid) <- 41    # and the size (radius) of the other layers
+m(lipid) <- 1.40+0i   #fixed value
 
 # load up silver data
-palik_ag_vis <- read.csv("palik_ag_vis.csv", header=FALSE)
-colnames(palik_ag_vis) <- c("lambda","n","k")
+palik_ag_vis <- read.table("palik_ag_vis_hb.csv", header=TRUE)
+colnames(palik_ag_vis) <- c("lambda","n","k","eps_real","eps_imag")
 
-lambda_palik <- palik_ag_vis$lambda*1000  #convert to nm
+lambda_palik <- palik_ag_vis$lambda #convert to nm
 n_palik <- palik_ag_vis$n 
 k_palik <- palik_ag_vis$k
 
 #interpolation as palik data is sparse
-n = 256 #number of points
+n = 512 #number of points
 spl_n <- approx(lambda_palik,n_palik,n=n)
 spl_k <- approx(lambda_palik,k_palik,n=n)
 lambda = spl_n$x
@@ -28,7 +33,7 @@ lambda = spl_n$x
 scatnlay_ar <-function(k){
   m(np) <- spl_n$y[k]+spl_k$y[k]*(0+1i);
   lambda(S) <- lambda[k];
-  St <- S+np;              # PUT THE STACK HERE
+  St <- S+np+water;              # PUT THE STACK HERE
   Q <- scattnlay(St);
 }
 
@@ -41,13 +46,23 @@ scatnlay_ar_lipid <-function(k){
 
 output <- sapply(1:n,scatnlay_ar)
 output_lipid <- sapply(1:n,scatnlay_ar_lipid)
-data_mat_np <- data.frame(t(output),lambda,1)
-data_mat_lipid <- data.frame(t(output_lipid),lambda,2)
+data_mat_np <- data.frame(t(output),lambda,'water')
+data_mat_lipid <- data.frame(t(output_lipid),lambda,'lipid')
 
 colnames(data_mat_np) <- c("Qext", "Qsca", "Qabs", "Qbk", "Qpr", "g", "Albedo", "nmax","Lambda","Layers")
 colnames(data_mat_lipid) <- c("Qext", "Qsca", "Qabs", "Qbk", "Qpr", "g", "Albedo", "nmax","Lambda","Layers")
 
 data_mat <- rbind(data_mat_np,data_mat_lipid)
 
-scat_plot <- ggplot(data=data_mat, aes(x=Lambda,y=Qsca, group=as.factor(Layers), color=as.factor(Layers)))
-scat_plot + geom_line() + theme_minimal()
+ext <- subset(data_mat_lipid, Lambda < 700, select= c("Qext","Lambda"))
+ext$Lambda[which.max(ext$Qext)]
+
+
+palik_ag_melt <- melt(subset(palik_ag_vis,select = c("lambda","n","k")), id=c("lambda"),variable.name = "type", 
+                      value.name = "Index")
+
+ag_plot <- ggplot(data=palik_ag_melt, aes(x=lambda,y=Index, group=type, color=type))
+ag_plot + geom_line() + theme_minimal(base_size=22) + xlim(c(300,700)) + ylim(c(0,6)) + xlab("Wavelength (nm)")
+
+scat_plot <- ggplot(data=data_mat, aes(x=Lambda,y=Qext, group=as.factor(Layers), color=as.factor(Layers))) 
+scat_plot + geom_line() + theme_minimal(base_size=22) + xlim(c(300,700)) + labs(colour = "Layers") 
