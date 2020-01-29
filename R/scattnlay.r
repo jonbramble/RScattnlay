9bdf1c3ce53592783d78d96ae1274cbbd59fee17
Wade Rogers <rogersw@upenn.edu>
2020-01-29 14:39:14 -0500

    Many configuration changes to create valid NAMESPACE and proper exports

--- R/scattnlay.r
+++ R/scattnlay.r
@@ -1,18 +1,38 @@
 #' @import Rcpp
+#' @importFrom methods new validObject
 NULL
 
-#' @title An S4 class to represent a layer around a nanoparticle
+#' An S4 class to represent a layer around a nanoparticle
 #'
 #' @slot m the complex refractive index
 #' @slot r the radius in nanometers
-#' @export
+#' @rdname Layer-class
+#' @export Layer
 Layer <- setClass("Layer",
                   slots=c(
                     m="complex",
                     r="numeric"
                     ))
 
-#' @title An S4 class to represent a scattering simulation
+#' @name r<-
+#' @rdname Layer-class
+#' @export
+setGeneric("r<-",function(x,value) standardGeneric("r<-"))
+#' @name m<-
+#' @rdname Layer-class
+#' @export
+setGeneric("m<-",function(x,value) standardGeneric("m<-"))
+
+setMethod("show", signature(object="Layer"),function(object){
+  cat("Layer Data \n")
+  cat("Diameter:", 2*object@r," nm\n")
+  cat("Complex Index:", object@m,"\n")
+})
+
+setMethod("r<-","Layer", function(x,value) {x@r <- value; validObject(x); x})
+setMethod("m<-","Layer", function(x,value) {x@m <- value; validObject(x); x})
+
+#' An S4 class to represent a scattering simulation
 #'
 #' @slot na the real refractive index of the ambient or surrounding media
 #' @slot lambda the wavelength of light in nanometers
@@ -20,7 +40,8 @@ Layer <- setClass("Layer",
 #' @slot ti the starting angle in degrees
 #' @slot tf the end angle in degrees
 #' @slot layers a list of layers of type Layer
-#' @export
+#' @rdname Scatterer-class
+#' @export Scatterer
 Scatterer <- setClass("Scatterer",
                slots=c(
                  na="numeric",
@@ -44,11 +65,26 @@ Scatterer <- setClass("Scatterer",
                 }
                )
 
-setMethod("show", signature(object="Layer"),function(object){
-  cat("Layer Data \n")
-  cat("Diameter:", 2*object@r," nm\n")
-  cat("Complex Index:", object@m,"\n")
-})
+#' @name na<-
+#' @rdname Scatterer-class
+#' @export
+setGeneric("na<-",function(x,value) standardGeneric("na<-"))
+#' @name lambda<-
+#' @rdname Scatterer-class
+#' @export
+setGeneric("lambda<-",function(x,value) standardGeneric("lambda<-"))
+#' @name nt<-
+#' @rdname Scatterer-class
+#' @export
+setGeneric("nt<-",function(x,value) standardGeneric("nt<-"))
+#' @name ti<-
+#' @rdname Scatterer-class
+#' @export
+setGeneric("ti<-",function(x,value) standardGeneric("ti<-"))
+#' @name tf<-
+#' @rdname Scatterer-class
+#' @export
+setGeneric("tf<-",function(x,value) standardGeneric("tf<-"))
 
 setMethod("show", signature(object="Scatterer"),function(object){
   cat("Scatterer Data \n")
@@ -56,38 +92,20 @@ setMethod("show", signature(object="Scatterer"),function(object){
   cat("Ambient Index", object@na, "\n")
 })
 
-setGeneric("na<-",function(x,value) standardGeneric("na<-"))
-setGeneric("lambda<-",function(x,value) standardGeneric("lambda<-"))
-setGeneric("nt<-",function(x,value) standardGeneric("nt<-"))
-setGeneric("ti<-",function(x,value) standardGeneric("ti<-"))
-setGeneric("tf<-",function(x,value) standardGeneric("tf<-"))
-setGeneric("r<-",function(x,value) standardGeneric("r<-"))
-setGeneric("m<-",function(x,value) standardGeneric("m<-"))
-
-
 setMethod("na<-","Scatterer", function(x,value) {x@na <- value; validObject(x); x})
 setMethod("lambda<-","Scatterer", function(x,value) {x@lambda <- value; validObject(x); x})
 setMethod("nt<-","Scatterer", function(x,value) {x@nt <- value; validObject(x); x})
 setMethod("ti<-","Scatterer", function(x,value) {x@ti <- value; validObject(x); x})
 setMethod("tf<-","Scatterer", function(x,value) {x@tf <- value; validObject(x); x})
-setMethod("r<-","Layer", function(x,value) {x@r <- value; validObject(x); x})
-setMethod("m<-","Layer", function(x,value) {x@m <- value; validObject(x); x})
 
 setGeneric("scattnlay", function(object) {standardGeneric("scattnlay")})
 
 #' Method amplitudes.
 #' @name amplitudes
 #' @rdname amplitudes-methods
-#' @exportMethod amplitudes
+#' @export
 setGeneric("amplitudes", function(object) {standardGeneric("amplitudes")})
 
-setMethod("scattnlay",signature(object="Scatterer"),function(object){
-  Rpp<-S4_SCATTNLAY(object)
-  return(Rpp)
-})
-
-#' @rdname amplitudes-methods
-#' @aliases amplitudes,ANY-method
 setMethod("amplitudes",signature(object="Scatterer"),function(object){
   S<-S4_AMPL(object)
   return(S)
@@ -97,4 +115,10 @@ setMethod("+", signature(e1="Scatterer",e2="Layer"), function(e1,e2){
   e1@layers <- c(e1@layers,e2)
   structure(e1,class="Scatterer")
 })
+
+setMethod("scattnlay",signature(object="Scatterer"),function(object){
+  Rpp<-S4_SCATTNLAY(object)
+  return(Rpp)
+})
+
 
